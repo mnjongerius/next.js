15a10308060f4e9dd318c9780b2dee5ed7ee3813
OJ Kwon <1210596+kwonoj@users.noreply.github.com.>
2024-02-02 15:29:38 -0800

    fff

--- packages/next-swc/crates/next-custom-transforms/src/transforms/react_server_components.rs
+++ packages/next-swc/crates/next-custom-transforms/src/transforms/react_server_components.rs
@@ -157,6 +157,8 @@ impl<C: Comments> VisitMut for ReactServerComponents<C> {
     noop_visit_mut_type!();
 
     fn visit_mut_module(&mut self, module: &mut Module) {
+        println!("rsc_transform {:#?}", self.filepath);
+
         let (is_client_entry, is_action_file, imports) =
             self.collect_top_level_directives_and_imports(module);
         let is_cjs = contains_cjs(module);
