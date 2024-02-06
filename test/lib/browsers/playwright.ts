732a31a05c87382302fcfb40fa90b99e5d24d60b
hrmny <8845940+ForsakenHarmony@users.noreply.github.com>
2024-02-06 04:24:57 +0100

    maybe better teardown

--- test/lib/browsers/playwright.ts
+++ test/lib/browsers/playwright.ts
@@ -50,11 +50,7 @@ export class Playwright extends BrowserInterface {
   private eventCallbacks: Record<Event, Set<(...args: any[]) => void>> = {
     request: new Set(),
   }
-  private async initContextTracing(
-    url: string,
-    page: Page,
-    context: BrowserContext
-  ) {
+  private async initContextTracing(url: string, context: BrowserContext) {
     if (!tracePlaywright) {
       return
     }
@@ -69,17 +65,13 @@ export class Playwright extends BrowserInterface {
         sources: true,
       })
       this.activeTrace = encodeURIComponent(url)
-
-      page.on('close', async () => {
-        await teardown(this.teardownTracing.bind(this))
-      })
     } catch (e) {
       this.activeTrace = undefined
     }
   }
 
   private async teardownTracing() {
-    if (!tracePlaywright || !this.activeTrace) {
+    if (!this.activeTrace) {
       return
     }
 
@@ -164,6 +156,7 @@ export class Playwright extends BrowserInterface {
   }
 
   async close(): Promise<void> {
+    await teardown(this.teardownTracing.bind(this))
     await page.close()
   }
 
@@ -206,12 +199,12 @@ export class Playwright extends BrowserInterface {
     }
   ) {
     // clean-up existing pages
-    for (const oldPage of context.pages()) {
-      await oldPage.close()
-    }
+    // for (const oldPage of context.pages()) {
+    //   await oldPage.close()
+    // }
 
+    await this.initContextTracing(url, context)
     page = await context.newPage()
-    await this.initContextTracing(url, page, context)
 
     // in development compilation can take longer due to
     // lower CPU availability in GH actions
